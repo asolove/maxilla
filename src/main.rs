53992f9d64cf039a8f409cbd0e885c154761f62a
Adam Solove <asolove@gmail.com>
2016-01-03 15:38:19 -0500

    Begin creating the http endpoint to return parse explanations of code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Parsing/explaining requirements, should be own module
 #![feature(rustc_private)]
 extern crate syntax;
 extern crate rustc;
@@ -16,12 +17,29 @@ mod explain;
 use explain::{SpannedExplainParse};
 mod tree;
 
+// Server requirements
+extern crate iron;
+extern crate bodyparser;
+
+use iron::prelude::*;
+use iron::status;
 
 fn main() {
-    let code = "fn main() { let greeting = greet(\"Dave\"); print!(greeing); }\nfn greet(name: &str) -> &str { \"Hello, \" + name }";
-    let krate = parse_code(code);
-    let explanation = krate.module.spanned_explain();
-    println!("{}", tree::annotate(explanation, code, 0));
+    Iron::new(|req: &mut Request| {
+      let mut code = "fn main() { let greeting = greet(\"Dave\"); print!(greeing); }\nfn greet(name: &str) -> &str { \"Hello, \" + name }".to_string();
+      let body = req.get::<bodyparser::Raw>();
+      match body {
+          Ok(Some(body)) => code = body,
+          // FIXME: error handling if nothing or invalid thing provided
+          Ok(None) => println!("No body"),
+          Err(err) => println!("Error: {:?}", err)
+      }
+      let krate = parse_code(&code);
+      let explanation = krate.module.spanned_explain();
+      // FIXME: parser currently just panics with invalid code. Catch and return a meaningful error.
+      let response = tree::annotate(explanation, &code, 0);
+        Ok(Response::with((status::Ok, response)))
+    }).http("localhost:3000").unwrap();
 }
 
 fn parse_code(code: &str) -> ast::Crate {
